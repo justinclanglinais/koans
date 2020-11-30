06b9152d8a1a779f7fca0315882e10380ec2e848
Justin Langlinais <justinclanglinais@gmail.com>
2020-11-30 12:39:55 -0800

    solving

--- about_methods.rb
+++ about_methods.rb
@@ -39,7 +39,7 @@ class AboutMethods < Neo::Koan
   # NOTE: wrong number of arguments is not a SYNTAX error, but a
   # runtime error.
   def test_calling_global_methods_with_wrong_number_of_arguments
-    exception = assert_raise(___) do
+    exception = assert_raise(ArgumentError) do
       my_global_method
     end
     assert_match(/__/, exception.message)
